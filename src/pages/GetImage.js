eb76f8755cda7a4c0ff75846d1fec7e781225e84
DiligentP <qkrwjdgus0603@gmail.com>
2023-07-17 20:54:57 +0900

    웹뷰와 통신 완료

--- src/pages/GetImage.js
+++ src/pages/GetImage.js
@@ -1,26 +1,47 @@
 import React, { useState, useEffect } from "react";
 
 const GetImage = () => {
-  const [imageData, setImageData] = useState(null);
+  const [data, setData] = useState(null);
 
   useEffect(() => {
-    const handleMessage = (event) => {
-      const message = event.data;
-      if (message.type === 'imageData') {
-        setImageData(message.data);
-      }
-    };
+    const userAgent = navigator.userAgent;
+    const isIOS = /iPhone|iPad|iPod/i.test(userAgent);
+    const receiver = isIOS ? window : document;
 
-    window.addEventListener('message', handleMessage);
+    // receiver를 사용하여 플랫폼에 따른 로직을 처리
+    receiver.addEventListener('message', handleMessage);
 
+    // 컴포넌트 언마운트 시 이벤트 리스너를 정리
     return () => {
-      window.removeEventListener('message', handleMessage);
+      receiver.removeEventListener('message', handleMessage);
     };
   }, []);
 
+  const handleMessage = (event) => {
+    const message = event.data;
+    setData(message)
+  };
+
+  // React -> React Native
+  // window.ReactNativeWebView.postMessage('Hello from WebView!');
+
+  // React Native -> React (Android)
+  // document.addEventListener('message', (event) => {
+  //     const receivedData = event.data;
+  //     // alert('Received data: ' + receivedData);
+  //     setData(receivedData)
+  // });
+
+  // React Native -> React (ios)
+  // window.addEventListener('message', (event) => {
+  //     const data = event.data;
+  //     alert('Received data:', data);
+  // });
+
   return (
     <>
-      {imageData}
+      <div> 전송 받은 이미지 경로 </div>
+      {data}
     </>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
+import React from "react";
 
-function Home () {
+function Home() {
     return (
         <>
-            <div style={{height: '100vh', width: '100%', textAlign: 'center', backgroundImage: `url("https://i.pinimg.com/564x/22/74/65/2274651970051f1526de8870973370e5.jpg")`, backgroundRepeat: 'no-repeat', backgroundPosition:'center', backgroundSize: 'cover' }}>
-                <h1 style={{paddingTop: 50, fontSize: 30, fontFamily: 'Noto Serif KR, serif'}}>오늘의 레시피 추천</h1>
-                <p style={{paddingTop: 20}}>Today's Food!</p>
+            <div style={{ height: '100vh', width: '100%', textAlign: 'center', backgroundImage: `url("https://i.pinimg.com/564x/22/74/65/2274651970051f1526de8870973370e5.jpg")`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'cover' }}>
+                <h1 style={{ paddingTop: 50, fontSize: 30, fontFamily: 'Noto Serif KR, serif' }}>오늘의 레시피 추천</h1>
+                <p style={{ paddingTop: 20 }}>Today's Food!</p>
             </div>
+
         </>
     )
 }
