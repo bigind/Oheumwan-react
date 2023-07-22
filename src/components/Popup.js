915737645ab7443bad135bef39695aef10b6203f
onlywonn <gpdnjsgp0282@gmail.com>
2023-07-22 16:42:01 +0900

    더보기 버튼 디자인추가

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,14 +5,14 @@ const Popup = ({ setPopupOpen}) => {
     };
 
     return (
-        <div className="container w-300 h-200 z-50 bg-slate-100 border-1 border-black rounded-lg absolute top-1/5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-            <button className="close absolute right-10" onClick={closePopup}>
+        <div className="container w-40 h-20 z-50 bg-white border-solid border-2 border-slate-200 rounded-lg absolute top-1/5 -right-20 transform -translate-x-1/2 -translate-y-1/2 text-center flex flex-col justify-center">
+            <button className="close absolute top-2 right-5" onClick={closePopup}>
                 X
             </button>
             <div>
-            <p>수정하기</p>
+            <p className="pb-2">수정하기</p>
             <hr/>
-            <p>삭제하기</p>
+            <p className="pt-2">삭제하기</p>
             </div>
         </div>
     );
--- src/pages/Router.js
+++ src/pages/Router.js
@@ -6,80 +6,80 @@ const Router = () => {
     return (
         <>
             <header>
-                <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
-                    <div class="sm:flex sm:items-center sm:justify-between">
-                        <div class="text-center sm:text-left">
-                            <h1 class="text-2xl font-bold text-gray-900 sm:text-3xl">
+                <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8">
+                    <div className="sm:flex sm:items-center sm:justify-between">
+                        <div className="text-center sm:text-left">
+                            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
                                 쉬운 이동을 위해 만든 라우팅 페이지 입니다.
                             </h1>
 
-                            <p class="mt-1.5 text-sm text-gray-500">
+                            <p className="mt-1.5 text-sm text-gray-500">
                                 Let's go !!! 🎉
                             </p>
                         </div>
                     </div>
                 </div>
             </header>
-            <div class="flex flex-col">
-                <div class="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
-                    <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                        <div class="overflow-hidden">
-                            <table class="min-w-full">
-                                <thead class="bg-white border-b">
+            <div className="flex flex-col">
+                <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
+                    <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="overflow-hidden">
+                            <table className="min-w-full">
+                                <thead className="bg-white border-b">
                                     <tr>
-                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                        <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                                             페이지 (클릭)
                                         </th>
-                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                        <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                                             URL
                                         </th>
-                                        <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                        <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
                                             설명
                                         </th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr class="bg-gray-100 border-b">
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    <tr className="bg-gray-100 border-b">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             <Link to="/home"> Home </Link>
                                         </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             /home
                                         </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             홈
                                         </td>
                                     </tr>
-                                    <tr class="bg-gray-100 border-b">
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    <tr className="bg-gray-100 border-b">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             <Link to="/community"> Community </Link>
                                         </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             /community
                                         </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             커뮤니티 페이지
                                         </td>
                                     </tr>
-                                    <tr class="bg-gray-100 border-b">
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    <tr className="bg-gray-100 border-b">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             <Link to="/profile"> Profile </Link>
                                         </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             /profile
                                         </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             프로필 페이지
                                         </td>
                                     </tr>
-                                    <tr class="bg-gray-100 border-b">
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    <tr className="bg-gray-100 border-b">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             <Link to="/getimage"> GetImage </Link>
                                         </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             /getimage
                                         </td>
-                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                             이미지 인식 페이지
                                         </td>
                                     </tr>
