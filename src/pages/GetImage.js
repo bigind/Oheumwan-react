3b96108aaeab50f56130ddb5bbef628a20629388
DiligentP <qkrwjdgus0603@gmail.com>
2023-07-25 00:02:31 +0900

    로딩 텍스트 변경

--- src/pages/GetImage.js
+++ src/pages/GetImage.js
@@ -42,7 +42,8 @@ const GetImage = () => {
                 <path
                     d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
               </svg>
-              Upload your file to our website
+              이미지 업로드 완료
+              {/*Upload your file to our website*/}
             </li>
             <li className="flex items-center">
               <svg className="w-4 h-4 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true"
@@ -50,7 +51,8 @@ const GetImage = () => {
                 <path
                     d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z"/>
               </svg>
-              Change the file size
+              서버가 이미지를 내려 받음
+              {/*Change the file size*/}
             </li>
             <li className="flex items-center">
               <div role="status">
@@ -66,7 +68,8 @@ const GetImage = () => {
                 </svg>
                 <span className="sr-only">Loading...</span>
               </div>
-              Extracting data from image
+              이미지 인식 중...
+              {/*Extracting data from image*/}
             </li>
           </ul>
           </div>
