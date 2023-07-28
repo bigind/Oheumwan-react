61354e03ba7c6a8ab802405c36e55dabe5c3ac07
DiligentP <qkrwjdgus0603@gmail.com>
2023-07-28 12:34:04 +0900

    카카오 로그인 페이지

--- src/pages/login/LoginAuth.js
+++ src/pages/login/LoginAuth.js
@@ -1,10 +1,46 @@
+import React, {useState} from "react";
+
 const LoginAuth = () => {
+
+    // useEffect(() => {
+    //     const params= new URL(document.location.toString()).searchParams;
+    //     const code = params.get('code');
+    //     const grantType = "authorization_code";
+    //     const REST_API_KEY = ``;
+    //     const REDIRECT_URI = `http://192.168.0.15:3000/auth`;
+    //
+    //     axios.post(
+    //         `https://kauth.kakao.com/oauth/token?grant_type=${grantType}&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&code=${code}`,
+    //         {},
+    //         { headers: { "Content-type": "application/x-www-form-urlencoded;charset=utf-8" } }
+    //     )
+    //         .then((res) => {
+    //             console.log(res);
+    //             const { access_token } = res.data;
+    //             axios.post(
+    //                 `https://kapi.kakao.com/v2/user/me`,
+    //                 {},
+    //                 {
+    //                     headers: {
+    //                         Authorization: `Bearer ${access_token}`,
+    //                         "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+    //                     }
+    //                 }
+    //             )
+    //                 .then((res) => {
+    //                     console.log('사용자 정보', res);
+    //                     setNickname(res.data.kakao_account.profile.nickname);
+    //                 })
+    //         })
+    //         .catch((Error) => {
+    //             console.log(Error);
+    //         })
+    //
+    // }, []);
+
+
     return(
-        <>
-            <div>
-                로그인 완료!
-            </div>
-        </>
+        <> </>
     )
 }
 export default LoginAuth;
